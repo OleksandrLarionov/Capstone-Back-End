2d39ccf38b6b3c5abfb0e624d11b5579956d4e1d
OleksandrLarionov <larionov.alex23@gmail.com>
2024-02-22 18:11:25 +0100

    fixed comments order

--- D-D-Blog/src/main/java/LarionovOleksandrBackEndCapstone/D/DBlog/repositories/CommentRepository.java
+++ D-D-Blog/src/main/java/LarionovOleksandrBackEndCapstone/D/DBlog/repositories/CommentRepository.java
@@ -4,6 +4,7 @@ import LarionovOleksandrBackEndCapstone.D.DBlog.entities.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    @Query("SELECT c FROM Comment c WHERE c.blogPost.id = :blogPostId ORDER BY c.date DESC")
     Page<Comment> findByBlogPostId(UUID blogPostId, Pageable pageable);
     List<Comment> findByUserId (UUID userId);
     List<Comment> findByBlogPostId (UUID blogPostId);
